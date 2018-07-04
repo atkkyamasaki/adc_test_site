2b14528721d9cdb0ac8450e7cc2ebbe79335a6e7
tyamasaki <tyamasaki@fortinet.com>
2018-07-04 16:10:36 +0900

    sleep処理を追加

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -16,6 +16,17 @@ class HomeController extends Controller
     {
         return $this->render('AppBundle:Home:index.html.twig');
     }
-}
 
+    /**
+     * @Route("/sleep")
+     */
+    public function sleepAction()
+    {
+        sleep(120);
+        // $test = phpinfo();
+        // error_log('debug = ' . print_r($test, true) . "\n", 3, 'C:\Users\Administrator\Desktop\debug.txt');
+        return $this->render('AppBundle:Home:index.html.twig');
+    }
+
+}
 
